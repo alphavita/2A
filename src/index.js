979ec4b1b834127bd5967da44ae4ba086d35beb9
alphavita <abcdef.vita2012@yandex.ru>
2016-11-10 19:43:41 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -18,3 +18,46 @@ app.get('/task2A',(req, res) => {
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
 });
+
+function capitalize(nameParts) {
+  let result = '';
+
+  if (nameParts.length === 3) {
+    result = nameParts[2] + ' ' + nameParts[0][0].toUpperCase() + '. ' + nameParts[1][0].toUpperCase() + '.';
+  } else if (nameParts.length === 2) {
+    result = nameParts[1] + ' ' + nameParts[0][0].toUpperCase() + '.';
+  } else {
+    result = nameParts[0];
+  }
+  return result;
+}
+
+app.get('/task2B', (req, res) => {
+  let response = '';
+
+  const fullname = req.query.fullname;
+  if (fullname === undefined || fullname.length === 0) {
+    response = 'Invalid fullname';
+  }
+
+  const  wer= /[0-9]/.test(fullname);
+
+  if (wer == true)
+  {
+      response = 'Invalid fullname';
+  }
+  else   if(fullname.length === 0) {
+     response = 'Invalid fullname';
+  }
+    else {
+    const nameParts = fullname.split(' ');
+    if (nameParts.length > 3 || nameParts.length === 0) {
+      response = 'Invalid fullname';
+    } else {
+      response = capitalize(nameParts);
+    }
+  }
+  res.send(response);
+});
+
+
